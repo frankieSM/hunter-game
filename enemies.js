819f3508a6d805eba0be62a527d5126d4c24fce4
franc <fmenendezwork@gmail.com>
2023-06-04 11:42:50 -0700

    working movement function in enemies.js

--- enemies.js
+++ enemies.js
@@ -1,20 +1,57 @@
 // REMEMBER TO LINK TO HTML
 
- class Duck {
-  constructor(image, pointValue, speed){
-    this.image = image;
-    this.pointValue = pointValue;
-    this.speed = speed;
-  }
- }
+const npc = newEnemy(450, 780)
 
-function spawnEnemy() {}
+function newImage(url){
+  let image = document.createElement('img')
+  image.src = url
+  image.style.position = 'absolute'
+  document.body.append(image)
+  return image
+}
+
+function newEnemy(x, y){
+  let element = newImage('assets/images/bird.jpg')
+  element.style.zIndex = 1;
+   
+  let spawnSide = 'right';
+
+    function moveEnemy(){
+      if(spawnSide === 'right'){
+        x -= 1
+      } else {
+        x += 1
+      }
+      element.style.left = x + 'px';
+      element.style.bottom = y + 'px';
+    }
+    setInterval(moveEnemy, 10)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  /* ENEMY VARIABLES: 
-   
     X COORDINATE
     Y COORDINATE
+    IMAGE
+    SPEED
+    POINTS
     DIRECTION = OPPOSITE SIDE FROM SPAWN SIDE
     SPAWN SIDE??? = LEFT OR RIGHT EDGE OF DISPLAY ?
-    EVENT LISTENER WITH DELETE FUNCTION
+    EVENT LISTENER WITH DELETE FUNCTION */
