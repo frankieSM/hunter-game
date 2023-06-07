2a9b533db6ed311b7c70d1575624c7c043aec4f8
franc <fmenendezwork@gmail.com>
2023-06-07 15:27:22 -0700

    edits to background image format

---
+++ random.js
@@ -0,0 +1,4 @@
+/* export const consoleLink = (thing) =>{
+    console.log(thing)
+} */
+
--- script.js
+++ script.js
@@ -20,17 +20,9 @@ function addBackButton(element){
 
 } 
 
-function toggleMenu(){
-    /*TODO: the back button is FUCKED. gonna need functions that add/remove
-            the menu while also adding/removing the rules. maybe store them 
-            i think maybe just declare the rules and menu globally.
-    */
-}
-
-
 function showRules() {
   clearMenu();
-  let rules = document.createElement("div");
+  let rules = document.createElement('div');
   rules.innerHTML =
     "RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES RULES";
   rules.style.backgroundColor = 'tan';
@@ -66,49 +58,49 @@ function toggleAudio(){
 */
 ///////////////////////////////////////////////////////////
 function spawnEnemy() {
-    var randomNumber = Math.random();
-    const enemy = document.createElement('img');
-  
-    enemy.addEventListener('click', () => {
-      enemy.remove();
-    });
-  
-    const gameDiv = document.getElementById('gameDiv');
-    var xCoord;
-    var yCoord = 500;
-    const spawnSide = getSpawnSide();
-  
-    enemy.src = 'assets/images/bird.jpg';
-    enemy.style.position = 'absolute';
-  
-    function getSpawnSide() {
-      //LEFT = -140px, RIGHT = 2550px
-      if (randomNumber < 0.5) {
-        xCoord = -140;
-        return 'left';
-      } else {
-        xCoord = 2550;
-        return 'right';
-      }
+  var randomNumber = Math.random();
+  const enemy = document.createElement('img');
+
+  enemy.addEventListener('click', () => {
+    enemy.remove();
+  });
+
+  const gameDiv = document.getElementById('gameDiv');
+  var xCoord;
+  var yCoord = Math.floor(Math.random() * 999) + 1;
+  const spawnSide = getSpawnSide();
+
+  enemy.src = 'assets/images/bird.jpg';
+  enemy.style.position = 'absolute';
+
+  function getSpawnSide() {
+    //LEFT = -140px, RIGHT = 2550px
+    if (randomNumber < 0.5) {
+      xCoord = -140;
+      return 'left';
+    } else {
+      xCoord = 2550;
+      return 'right';
     }
-  
-    function moveEnemy() {
-      enemy.style.zIndex = 1;
-      if (spawnSide === 'right') {
-        xCoord -= 2; // increase value to change speed
-      } else {
-        xCoord += 2; 
-      }
-      enemy.style.left = xCoord + 'px'; // updates the left position
-    }
-  
-    getSpawnSide(); 
-    enemy.style.left = xCoord + 'px';
-    enemy.style.bottom = yCoord + 'px';
-    gameDiv.appendChild(enemy);
-  
-    setInterval(moveEnemy, 1); // Start the enemy movement
   }
+
+  function moveEnemy() {
+    enemy.style.zIndex = 1;
+    if (spawnSide === 'right') {
+      xCoord -= 2; // increase value to change speed
+    } else {
+      xCoord += 2;
+    }
+    enemy.style.left = xCoord + 'px'; // updates the left position
+  }
+
+  getSpawnSide();
+  enemy.style.left = xCoord + 'px';
+  enemy.style.bottom = yCoord + 'px';
+  gameDiv.appendChild(enemy);
+
+  setInterval(moveEnemy, 1); // Start the enemy movement
+}
   //////////////////////////////////////////////////////////////////
   function startGame() {
     const gameDiv = document.createElement('div');
